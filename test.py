f7c5c1155242a9e046d7e55b81bdd592be6456a9
Jesus Vasquez <jesusvasquez333@gmail.com>
2018-03-19 20:43:57 -0700

    Add test for sum of floats

--- test.py
+++ test.py
@@ -14,5 +14,12 @@ class TestSum(unittest.TestCase):
         result = maths.sum(3,5)
         self.assertEqual(result, 8)
 
+    def test_sum_floats(self):
+        """
+        Test the sum of floats
+        """
+        result = maths.sum(15.3,27.8)
+        self.assertEqual(43.1)
+
 if __name__ == '__main__':
     unittest.main()
