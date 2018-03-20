fbd56be61f630316ea7779a49b6ecf053e0f5905
Jesus Vasquez <jesusvasquez333@gmail.com>
2018-03-19 20:55:16 -0700

    Add test for subtraction function

--- test.py
+++ test.py
@@ -21,5 +21,14 @@ class TestSum(unittest.TestCase):
         result = maths.sum(15.3,27.8)
         self.assertEqual(result, 43.1)
 
+class TestSubs(unittest.TestCase):
+    """
+    Test the subtraction function
+    """
+
+    def test_subtract_integers(self):
+        result = maths.subtract(15,7):
+        self.assertEqual(result,8)
+
 if __name__ == '__main__':
     unittest.main()
