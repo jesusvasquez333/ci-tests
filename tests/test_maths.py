a30f2dd7cc18410e915136e92863b9145adc6189
Jesus Vasquez <jesusvasquez333@gmail.com>
2018-09-04 22:39:27 -0700

    Import maths function from new package.

--- tests/test_maths.py
+++ tests/test_maths.py
@@ -1,34 +1,34 @@
 #!/usr/bin/env python
 
 import pytest
-import maths
+import mathpack.maths as m
 
 def test_sum_integers():
     """
     Test the addition of integers
     """
-    assert(maths.addition(3,5) == 8)
+    assert(m.addition(3,5) == 8)
 
 def test_sum_floats():
     """
     Test the addition of floats
     """
-    assert(maths.addition(15.3,27.8) == 43.1)
+    assert(m.addition(15.3,27.8) == 43.1)
 
 def test_subtract_integers():
     """
     Test the substraction function
     """
-    asser(maths.subtract(15,7) == 8)
+    assert(m.subtract(15,7) == 8)
 
 def test_multiply_integers():
     """
     Test the multiplication function
     """
-    asset(maths.multiply(8,7) == 56)
+    assert(m.multiply(8,7) == 56)
 
 def test_division_integers():
     """
     Test the division function
     """
-    assert(maths.divide(20,8) == 2.5)
+    assert(m.divide(20,8) == 2.5)
